213b4f5086b9a3572994ce5f311acf7d6b09d84e
Anton Golub <golub.anton@gmail.com>
2020-07-31 23:11:11 +0400

    feat: perform most operations on temporary entities
    closes #6

--- src/main/ts/index.ts
+++ src/main/ts/index.ts
@@ -1,56 +1,64 @@
 import fs from 'fs-extra'
 import synp from 'synp'
+import {join} from 'path'
+import findCacheDir from 'find-cache-dir'
 import {
   invoke,
   promisify,
   asyncForEach,
 } from './util'
 
-type TContext = { cwd: string }
+type TContext = { cwd: string, temp: string }
 
 type TCallback = (cxt: TContext) => void
 
 type TStage = [string, ...TCallback[]]
 
-const fixWorkspaces: TCallback = ({cwd}: TContext) => {
-  // save original file for rollback
-  fs.copyFileSync('package.json', 'origin.package.json')
-
+const fixWorkspaces: TCallback = ({temp}) => {
   // https://github.com/antongolub/yarn-audit-fix/issues/2
-  const pkgJsonData = JSON.parse(fs.readFileSync('package.json', 'utf-8').trim())
+  const pkgJsonData = JSON.parse(fs.readFileSync(join(temp, 'package.json'), 'utf-8').trim())
   delete pkgJsonData.workspaces
 
-  fs.writeFileSync('package.json', JSON.stringify(pkgJsonData, null, 2))
+  fs.writeFileSync(join(temp, 'package.json'), JSON.stringify(pkgJsonData, null, 2))
 }
 
-const clearTempData: TCallback = () => {
-  fs.copyFileSync('origin.package.json', 'package.json')
-  fs.removeSync('origin.package.json')
-  fs.removeSync('package-lock.json')
+const createTempAssets: TCallback = ({temp}) => {
+  fs.copyFileSync('yarn.lock', join(temp, 'yarn.lock'))
+  fs.copyFileSync('package.json', join(temp, 'package.json'))
+  fs.createSymlinkSync('node_modules', join(temp, 'node_modules'), 'dir')
+}
+
+const clear: TCallback = ({temp}) => {
+  fs.emptyDirSync(temp)
 }
 
 const applyYarn: TCallback = ({cwd}) =>
   invoke('yarn', [], cwd)
 
-const yarnImport: TCallback = ({cwd}) => {
-  fs.removeSync('yarn.lock')
-  invoke('yarn', ['import'], cwd)
+const yarnImport: TCallback = ({temp}) => {
+  invoke('yarn', ['import'], temp)
+  fs.copyFileSync(join(temp, 'yarn.lock'), 'yarn.lock')
 }
 
-const npmAuditFix: TCallback = ({cwd}) =>
-    invoke('npm', ['audit', 'fix', '--package-lock-only'], cwd)
+const npmAuditFix: TCallback = ({temp}) =>
+    invoke('npm', ['audit', 'fix', '--package-lock-only'], temp)
 
-const yarnLockToPkgLock: TCallback = ({cwd}) => {
-  const pgkLockJsonData = synp.yarnToNpm(cwd)
+const yarnLockToPkgLock: TCallback = ({temp}) => {
+  const pgkLockJsonData = synp.yarnToNpm(temp)
 
-  fs.writeFileSync('package-lock.json', pgkLockJsonData)
+  fs.writeFileSync(join(temp, 'package-lock.json'), pgkLockJsonData)
+  fs.removeSync(join(temp, 'yarn.lock'))
 }
 
 export const stages: TStage[] = [
   [
-    'Generating package-lock.json from yarn.lock...',
-    applyYarn,
+    'Preparing temp assets...',
+    clear,
+    createTempAssets,
     fixWorkspaces,
+  ],
+  [
+    'Generating package-lock.json from yarn.lock...',
     yarnLockToPkgLock,
   ],
   [
@@ -60,8 +68,8 @@ export const stages: TStage[] = [
   [
     'Updating yarn.lock from package-lock.json...',
     yarnImport,
-    clearTempData,
     applyYarn,
+    clear,
   ],
   [
     'Done',
@@ -69,7 +77,10 @@ export const stages: TStage[] = [
 ]
 
 export const run = async() => {
-  const cxt = {cwd: process.cwd()}
+  const cxt = {
+    cwd: process.cwd(),
+    temp: findCacheDir({name: 'yarn-audit-fix', create: true}) + '',
+  }
 
   return asyncForEach(stages, async([description, ...cbs]) => {
     console.log(description)
