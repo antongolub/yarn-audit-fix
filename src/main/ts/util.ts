dfe82fb4de711dcddc0feb497c7ade66d4ef717d
Anton Golub <golub.anton@gmail.com>
2020-08-07 14:33:08 +0400

    fix: print invoke cmd before its output

--- src/main/ts/util.ts
+++ src/main/ts/util.ts
@@ -2,9 +2,9 @@ import cp from 'child_process'
 import chalk from 'chalk'
 
 export const invoke = (cmd: string, args: string[], cwd: string) => {
-  const result = cp.spawnSync(cmd, args, {cwd, stdio: ['inherit', 'inherit', 'inherit']})
+  console.log(chalk.bold('invoke'), cmd, ...args)
 
-  console.log(chalk.bold('invoke:'), cmd, ...args)
+  const result = cp.spawnSync(cmd, args, {cwd, stdio: ['inherit', 'inherit', 'inherit']})
 
   if (result.error || result.status) {
     throw result
