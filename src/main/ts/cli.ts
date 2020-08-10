67960e9bbe48062f844302bea99d23a5cb8c92c2
Anton Golub <golub.anton@gmail.com>
2020-08-10 11:50:04 +0400

    test(cli): add test for runtime exception

--- src/main/ts/cli.ts
+++ src/main/ts/cli.ts
@@ -7,5 +7,5 @@ const flags = parseFlags(process.argv.slice(2))
 
 run(flags).catch(reason => {
   !flags.silent && console.error(reason)
-  process.exit(+reason.status || 1)
+  process.exit(reason.status | 0 || 1)
 })
