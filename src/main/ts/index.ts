96bde5d7ad237bc9bc56c7820d9a1529fded6e02
Anton Golub <golub.anton@gmail.com>
2020-08-18 14:52:52 +0400

    feat: support custom workspace paths

--- src/main/ts/index.ts
+++ src/main/ts/index.ts
@@ -3,7 +3,7 @@ import synp from '@antongolub/synp'
 import {join} from 'path'
 import findCacheDir from 'find-cache-dir'
 import chalk from 'chalk'
-import {invoke, formatFlags, getSymlinkType, getNpmBin} from './util'
+import {invoke, formatFlags, getSymlinkType, getNpmBin, getWorkspaces} from './util'
 
 type TContext = { cwd: string, temp: string, flags: Record<string, any> }
 
@@ -19,10 +19,25 @@ type TStage = [string, ...TCallback[]]
 const createTempAssets: TCallback = ({temp, flags}) => {
   fs.copyFileSync('yarn.lock', join(temp, 'yarn.lock'))
   fs.copyFileSync('package.json', join(temp, 'package.json'))
-  fs.createSymlinkSync('node_modules', join(temp, 'node_modules'), getSymlinkType(flags.symlink) as SymlinkType) // TODO fix fs-extra typings issue
-  if (fs.existsSync('packages')) {
-    fs.createSymlinkSync('packages', join(temp, 'packages'), getSymlinkType(flags.symlink) as SymlinkType)
-  }
+}
+
+/**
+ * Provide symlinks to node_modules and workspaces
+ * @param {TContext} cxt
+ * @return {void}
+ */
+const createSymlinks: TCallback = ({temp, flags, cwd}) => {
+  const symlinkType = getSymlinkType(flags.symlink) as SymlinkType // TODO fix fs-extra typings issue
+  const workspaces = getWorkspaces(cwd)
+  const links = [join(cwd, 'node_modules'), ...workspaces]
+
+  links.forEach((pkgPath: string) => {
+    const rel = pkgPath.replace(/\/package\.json$/, '').slice(cwd.length)
+    const from = join(cwd, rel)
+    const to = join(temp, rel)
+
+    fs.createSymlinkSync(from, to, symlinkType)
+  })
 }
 
 /**
@@ -56,8 +71,8 @@ const npmAuditFix: TCallback = ({temp, flags}) => {
  */
 const yarnImport: TCallback = ({temp, flags}) => {
   const yarnLockData = synp.npmToYarn(temp)
+
   fs.writeFileSync(join(temp, 'yarn.lock'), yarnLockData)
-  // invoke('yarn', ['import', ...formatFlags(flags, 'verbose', 'silent')], temp, flags.silent)
   fs.copyFileSync(join(temp, 'yarn.lock'), 'yarn.lock')
 }
 
@@ -82,6 +97,7 @@ export const stages: TStage[] = [
     'Preparing temp assets...',
     clear,
     createTempAssets,
+    createSymlinks,
   ],
   [
     'Generating package-lock.json from yarn.lock...',
