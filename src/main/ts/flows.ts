fbda8d91a4452a4b9bc505238a5579b7067f79a1
Anton Golub <antongolub@antongolub.com>
2022-01-20 17:29:08 +0300

    refactor: simplify internal flow decraration, let flows be nested

--- src/main/ts/flows.ts
+++ src/main/ts/flows.ts
@@ -18,41 +18,41 @@ import {
 // Compose `npm audit fix` with lockfile converter.
 export const convert: TFlow = {
   main: [
-    ['Resolve bins', resolveBins],
-    ['Runtime digest', printRuntimeDigest],
-    ['Verifying package structure...', verify],
-    ['Preparing temp assets...', clear, createTempAssets, createSymlinks],
-    ['Generating package-lock.json from yarn.lock...', yarnLockToPkgLock],
-    ['Applying npm audit fix...', npmAuditFix],
+    'Resolve bins', resolveBins,
+    'Runtime digest', printRuntimeDigest,
+    'Verifying package structure...', verify,
+    'Preparing temp assets...', clear, createTempAssets, createSymlinks,
+    'Generating package-lock.json from yarn.lock...', yarnLockToPkgLock,
+    'Applying npm audit fix...', npmAuditFix,
     [
       'Updating yarn.lock from package-lock.json...',
       yarnImport,
       syncLockfile,
       clear,
     ],
-    ['Installing deps update...', yarnInstall],
-    ['Done'],
+    'Installing deps update...', yarnInstall,
+    'Done',
   ],
-  fallback: [['Failure!', clear, exit]],
+  fallback: ['Failure!', clear, exit],
 }
 
 // Inject `yarn audit --json` data to lockfile.
 export const patch: TFlow = {
   main: [
-    ['Resolve bins', resolveBins],
-    ['Runtime digest', printRuntimeDigest],
-    ['Verifying package structure...', verify],
-    ['Preparing temp assets...', clear, createTempAssets, createSymlinks],
+    'Resolve bins', resolveBins,
+    'Runtime digest', printRuntimeDigest,
+    'Verifying package structure...', verify,
+    'Preparing temp assets...', clear, createTempAssets, createSymlinks,
     [
       'Patching yarn.lock with audit data...',
       patchLockfile,
       syncLockfile,
       clear,
     ],
-    ['Installing deps update...', yarnInstall],
-    ['Done'],
+    'Installing deps update...', yarnInstall,
+    'Done',
   ],
-  fallback: [['Failure!', clear, exit]],
+  fallback: ['Failure!', clear, exit],
 }
 
 // Select `yarn.lock` modification strategy.
